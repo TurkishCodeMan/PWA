79282ab3ed4952c17c018353ff40f612b44dc168
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2023-03-18 21:13:49 +0300

    Revized Sign in

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,9 +1,4 @@
 
-import { Button } from "@/shared/components/button";
-import style from "./style.module.scss";
-import { Facebook, Instagram, Linkedin } from "react-feather";
-import { Input } from "@/shared/components/input";
-import { useRegister } from "@/shared/api/auth";
 import AuthForm from "@/features/auth/auth-form";
 export default function Signin() {
 
