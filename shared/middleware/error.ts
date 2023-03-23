23de0881c6ff6b6446d2e71ebd91d4b24f62897a
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2023-03-23 22:47:32 +0300

    add entry set

--- shared/middleware/error.ts
+++ shared/middleware/error.ts
@@ -1,4 +1,4 @@
-export default async function onError(error, req, res, next) {
+export default async function onError(error:any, req:any, res:any, next:any) {
   console.log(error)
   res.status(500).end()
 }
