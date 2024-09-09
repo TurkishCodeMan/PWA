1f06af0fe084180bd24c14725e8c31d1a1787b01
Hilal Karaman <02180201039@ogr.inonu.edu.tr>
2024-09-09 17:49:22 +0300

    profile page finish

--- app/(employee)/welcome/page.tsx
+++ app/(employee)/welcome/page.tsx
@@ -3,6 +3,6 @@ import { Employee } from "@/features/employee";
 
 export default function Welcome() {
     return (
-        <Employee.welcome />
+        <Employee.Welcome />
     )
 }
--- features/employee/profile/index.tsx
+++ features/employee/profile/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import style from "./style.module.scss";
 import { IoMdArrowBack } from "react-icons/io";
-// import { RiRobot3Line } from "react-icons/ri";
+import { FaRobot } from "react-icons/fa";
+import { Field, Input, Label } from '@headlessui/react';
+import { SlPencil } from "react-icons/sl";
+import { VscSignOut } from "react-icons/vsc";
+import { AiOutlineHome } from "react-icons/ai";
+import { GoClock } from "react-icons/go";
+import { MdOutlineHistory } from "react-icons/md";
+import { BsClock } from "react-icons/bs";
 
 export default function Profile() {
     return (
@@ -10,20 +17,75 @@ export default function Profile() {
             <div className={style["back-icon"]}>
                 <IoMdArrowBack size={25} />
             </div>
-            <div className={style["profile-img-card"]}>
-                <div className={style["profile-img"]}>
-                    {/* <RiRobot3Line /> */}
+            <div className={style["profile-list"]}>
+                <div className={style["profile-img-card"]}>
+                    <div className={style["profile-img"]}>
+                        <div className={style["status"]}></div>
+                        <FaRobot color="#A4A1BC" size={50} />
+                    </div>
+                    <div className={style["employee-name"]}>
+                        <h3>Hüseyin Altikulac</h3>
+                        <div>
+                            <p className={style["employee-title"]}>Employee : </p>
+                            <p>Service</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={style["employee-name"]}>
-                    <h3>Hüseyin Altikulac</h3>
-                    <div>
-                        <p className={style["employee-title"]}>Employee : </p>
-                        <p>Service</p>
+                <div className={style["profile-info"]}>
+                    <div className={style["name"]}>
+                        <Field className={style["field"]}>
+                            <Label>Profile name</Label>
+                            <div className={style["name-input"]}>
+                                <Input name="full_name" placeholder="Huseyin Altikulac" />
+                                <div className={style["pencil-icon"]}>
+                                    <SlPencil size={18} />
+                                </div>
+                            </div>
+                        </Field>
+                    </div>
+                    <div className={style["name"]}>
+                        <Field className={style["field"]}>
+                            <Label>Email</Label>
+                            <div className={style["name-input"]}>
+                                <Input name="full_name" placeholder="SeloYa@Hotmail.com" />
+                                <div className={style["pencil-icon"]}>
+                                    <SlPencil size={18} />
+                                </div>
+                            </div>
+                        </Field>
+                    </div>
+                    <div className={style["name"]}>
+                        <Field className={style["field"]}>
+                            <Label>Password</Label>
+                            <div className={style["name-input"]}>
+                                <Input name="full_name" placeholder="* * * *" />
+                                <div className={style["pencil-icon"]}>
+                                    <SlPencil size={18} />
+                                </div>
+                            </div>
+                        </Field>
                     </div>
                 </div>
             </div>
-            <div className={style["profile-info"]}>
-                
+            <div className={style["sign-out-list"]}>
+                <div className={style["sign-out"]}>
+                    <VscSignOut color=" #262753" size={28} />
+                    <h3>Sign outr</h3>
+                </div>
+            </div>
+            <div className={style["footer"]}>
+                <div className={style["home"]}>
+                    <AiOutlineHome size={35} />
+                    <p>Home</p>
+                </div>
+                <div className={style["hours"]}>
+                    <BsClock size={33} />
+                    <p>Hours</p>
+                </div>
+                <div className={style["history"]}>
+                    <MdOutlineHistory size={35} />
+                    <p>History</p>
+                </div>
             </div>
         </div>
     )
